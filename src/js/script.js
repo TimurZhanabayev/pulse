220b92a38de968143f80b2dccb9be7e44b5459be
Timur <zhanabayev.98@gmail.com>
2022-02-28 01:19:31 +0600

    Validation

--- src/js/script.js
+++ src/js/script.js
@@ -31,4 +31,54 @@ $(document).ready(function(){
     };
     toggleSlide('.link_detail');
     toggleSlide('.link_back');
+
+    //modal
+
+    $('[data-modal=consult]').on('click', function() {
+        $('.overlay, #consult').fadeIn();
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consult, #order, #thanks').fadeOut();
+    });
+    $('.button_buy').each(function(i) {
+        $(this).on('click', function() { 
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn();
+        })
+    });
+
+    //validate
+
+    function valideForms(form){
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста введите свое имя",
+                    minlength: jQuery.validator.format("Введите {0} символа!")
+                },
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                    required: "Пожалуйста введите свою почту",
+                    email: "Неправильно введен адрес почты"
+                }
+            }
+        });
+    };
+
+    valideForms('#consultation-form');
+    valideForms('#consult form');
+    valideForms('#order form');
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99",{autoclear: false});
 });
